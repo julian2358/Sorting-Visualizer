1efabb212ec65789598aa65a819dff85554f7aed
julian2358 <julianreeves2358@gmail.com>
2020-07-13 12:32:57 -0400

    push

---
+++ src/SortingAlgorithm/SortingAlgorithm.js
@@ -0,0 +1,97 @@
+//merge sort algorithm
+//basically at any given point in time we find the middle index of the array we are dealing with
+//we call mergeSort recursively on the first half and then on the second half    
+//then we iiterate through the first and second half and we take the smallest number everytime and merge the two arrays 
+//had to scrap the original version where we were creating new smaller and smaller arrays because it could not keep track of animations
+
+
+
+//we need to know what place in the orginal array were at when were comparing 2 values
+//because were going to grab the array-bar in the DOM that corressponds to that position and change its color/height value
+//original version where we were creating new smaller and smaller arrays
+//realized we need indicies that keep track of where we are in merge sort
+
+
+//this merge sort uses an auxiallary array where you can override values
+export function getMergeSortAnimations(array) {
+    const animations = [];
+    if (array.length <= 1) return array;
+    const auxiliaryArray = array.slice();
+    mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
+    return animations;
+  }
+  
+  function mergeSortHelper(
+    mainArray,
+    startIdx,
+    endIdx,
+    auxiliaryArray,
+    animations,
+  ) {
+    if (startIdx === endIdx) return;
+    const middleIdx = Math.floor((startIdx + endIdx) / 2);
+    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+    mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+  }
+  
+  //At every point of domerge we add an animation into our animations array
+  //whenever we compare values create an animation that will have a comparison property that will hold the two values
+  // [i, j] those are going to be the two values that we want to color at any given point in time
+
+  function doMerge(
+    mainArray,
+    startIdx,
+    middleIdx,
+    endIdx,
+    auxiliaryArray,
+    animations,
+  ) {
+    let k = startIdx;
+    let i = startIdx;
+    let j = middleIdx + 1;
+    while (i <= middleIdx && j <= endIdx) {
+      // These are the values that we're comparing; we push them once
+      // to change their color.
+      animations.push([i, j]);
+      // These are the values that we're comparing; we push them a second
+      // time to revert their color.
+      animations.push([i, j]);
+      if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+        // We overwrite the value at index k in the original array with the
+        // value at index i in the auxiliary array.
+        animations.push([k, auxiliaryArray[i]]);
+        mainArray[k++] = auxiliaryArray[i++];
+      } else {
+        // We overwrite the value at index k in the original array with the
+        // value at index j in the auxiliary array.
+        animations.push([k, auxiliaryArray[j]]);
+        mainArray[k++] = auxiliaryArray[j++];
+      }
+    }
+    while (i <= middleIdx) {
+      // These are the values that we're comparing; we push them once
+      // to change their color.
+      animations.push([i, i]);
+      // These are the values that we're comparing; we push them a second
+      // time to revert their color.
+      animations.push([i, i]);
+      // We overwrite the value at index k in the original array with the
+      // value at index i in the auxiliary array.
+      animations.push([k, auxiliaryArray[i]]);
+      mainArray[k++] = auxiliaryArray[i++];
+    }
+    while (j <= endIdx) {
+      // These are the values that we're comparing; we push them once
+      // to change their color.
+      animations.push([j, j]);
+      // These are the values that we're comparing; we push them a second
+      // time to revert their color.
+      animations.push([j, j]);
+      // We overwrite the value at index k in the original array with the
+      // value at index j in the auxiliary array.
+      animations.push([k, auxiliaryArray[j]]);
+      mainArray[k++] = auxiliaryArray[j++];
+    }
+  }
+ 
---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import './SortingVisualizer.css';
+import * as SortingAlgorithms from '../SortingAlgorithm/SortingAlgorithm'
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 310;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'turquoise';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
+
+
+
+export default class SortingVisualizer extends React.Component{
+    constructor(props){
+        super(props);
+
+        //store main array in state
+        this.state = {
+            array: [],
+
+        }
+    };
+//when this component loads call resetarray()
+    componentDidMount(){ 
+        this.resetArray();
+    }
+//Creates an array and itterates 100, pushes random values between 5, 1000
+    resetArray() {
+        const array = [];
+        for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+          array.push(randomIntFromInterval(5, 700));
+        }
+        this.setState({array});
+      }
+    
+      mergeSort() {
+        const animations = SortingAlgorithms.getMergeSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+          const arrayBars = document.getElementsByClassName('array-bar');
+          const isColorChange = i % 3 !== 2;
+          if (isColorChange) {
+            const [barOneIdx, barTwoIdx] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+            setTimeout(() => {
+              barOneStyle.backgroundColor = color;
+              barTwoStyle.backgroundColor = color;
+            }, i * ANIMATION_SPEED_MS);
+          } else {
+            setTimeout(() => {
+              const [barOneIdx, newHeight] = animations[i];
+              const barOneStyle = arrayBars[barOneIdx].style;
+              barOneStyle.height = `${newHeight}px`;
+            }, i * ANIMATION_SPEED_MS);
+          }
+        }
+      }
+    
+
+
+
+
+    render() {
+        const {array} = this.state;
+
+        return (
+           <div className="array-container">
+
+            {array.map((value, idx) => (
+                <div className="array-bar" 
+                key={idx}
+                style={{height: `${value}px`}}></div>
+                
+            ))}
+            <button onClick={() => this.resetArray()}>Generate New Array</button>
+            <button onClick={() => this.mergeSort()}>Merge Sort</button>
+            </div>
+        );
+    }
+}
+
+    function randomIntFromInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
